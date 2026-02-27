a02e9da18617190250b5d172548bfd3a33f8754a
houshengzyzn <housheng_zyzn@outlook.com>
2026-02-27 14:31:40 +0800

    fix(dev_ide): Element Plus 分页文案跟随语言切换

--- dev_ide/src/App.vue
+++ dev_ide/src/App.vue
@@ -1,13 +1,22 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <el-config-provider :locale="elementLocale">
+    <div id="app">
+      <router-view />
+    </div>
+  </el-config-provider>
 </template>
 
-<script>
-export default {
-  name: 'App',
-}
+<script setup>
+import { computed } from 'vue'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
+import en from 'element-plus/es/locale/lang/en'
+import { useAppStore } from '@/store'
+
+const appStore = useAppStore()
+
+const elementLocale = computed(() => {
+  return appStore.language === 'en' ? en : zhCn
+})
 </script>
 
 <style>
